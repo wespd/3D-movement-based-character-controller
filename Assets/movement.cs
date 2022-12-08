0c1a8b3032cf17a8c754d6544519908e6f358d04
wespd <37629858+wespd@users.noreply.github.com>
2022-12-07 17:28:17 -0800

    improved movement system

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -14,36 +14,57 @@ public class movement : MonoBehaviour
     public float jumpStrength;
     public float jumpDetectionHeight;
     public float maxDistance;
-
+    [Range(0,1)]
+    public float frictionAmount;
+    [Range(1,10)]
+    public float notMovingFrictionMultiplier;
     // Update is called once per frame
     void Update()
     {
+        Vector3 movement = Vector3.zero;
+
         float acceleration = speed/Mathf.Max(rB.velocity.magnitude, 1); 
         //rB.velocity = new Vector3(0, rB.velocity.y, 0);
         if(isGrounded())
         {
-                if(Input.GetKey(forward))
-            {
-                rB.AddForce(transform.forward * acceleration);
-            }
-            if(Input.GetKey(backward))
-            {
-                rB.AddForce(-transform.forward * acceleration);
-            }
-            if(Input.GetKey(left))
-            {
-                rB.AddForce(-transform.right * acceleration);
-            }
-            if(Input.GetKey(right))
-            {
-                rB.AddForce(transform.right * acceleration);
-            }
+
                 if(Input.GetKeyDown(jump))
             {
                 rB.AddForce(transform.up * jumpStrength, ForceMode.Impulse);
             }
         }
+        if(Input.GetKey(forward))
+        {
+            movement += transform.forward;
+        }
+        if(Input.GetKey(backward))
+        {
+            movement -= transform.forward;
+        }
+        if(Input.GetKey(left))
+        {
+            movement -= transform.right;
+        }
+        if(Input.GetKey(right))
+        {
+            movement += transform.right;
+        }
+        if(movement == Vector3.zero)
+        {
+            frictionAmount *= notMovingFrictionMultiplier;
+        }
+        rB.AddForce(movement * acceleration, ForceMode.VelocityChange);
+        rB.AddForce(new Vector3(-rB.velocity.x, 0, -rB.velocity.z) * frictionAmount);
+        if(movement == Vector3.zero)
+        {
+            frictionAmount /= notMovingFrictionMultiplier;
+        }
+        if(rB.velocity.x <= 0.01)
+        {
+            //rB.volcity = new Vector3();
+        }
     }
+    
     public bool isGrounded()
     {
         RaycastHit hit;
